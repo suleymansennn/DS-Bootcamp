9e464d3944de9f86b4e1a1bef33e6686d741d6d9
Süleyman Şen <83352965+suleymansennn@users.noreply.github.com>
2022-10-17 16:56:40 +0300

    Update persona_streamlit.py

--- week3/persona_streamlit.py
+++ week3/persona_streamlit.py
@@ -46,7 +46,7 @@ def show_result(new_user):
 
 
 st.title("Customer Level Based Segmentation")
-img = Image.open("https://raw.githubusercontent.com/suleymansennn/DS-Bootcamp/main/week3/customer.jpeg")
+img = "https://raw.githubusercontent.com/suleymansennn/DS-Bootcamp/main/week3/customer.jpeg"
 st.image(img)
 
 age = st.number_input("Enter your age", step=1)
